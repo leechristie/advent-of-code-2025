614b95d91da06a42ef05be8cb09b01b1784a5ae2
Dr Lee A. Christie <l.a.christie@rgu.ac.uk>
2025-11-23 20:52:12 +0000

    Update grid.py

--- structures/grid.py
+++ structures/grid.py
@@ -1,66 +1,142 @@
 from __future__ import annotations
 
+import numpy as np
+
 from typing import TextIO
 
-from structures.points import Dimensions, Point, Facing
+from structures.points import Dimensions, Point
 
 
 class Grid:
 
-    cells: list[list[str]]
+    cells: np.ndarray
     dimensions: Dimensions
-    floor: str | None
+    symbols: tuple[str, ...]
 
-    def __init__(self, rows: list[str], floor: str|None=None) -> None:
-        assert type(rows) == list and len(rows) > 0
-        assert type(floor) == str and len(floor) == 1
-        self.floor = floor
-        self.cells = []
-        width: int | None = None
-        for row in rows:
-            row_cells: list[str] = []
-            assert type(row) == str
-            if width is not None:
-                assert len(row) == width
-            width = len(row)
-            assert '\n' not in row
-            for cell in row:
-                row_cells.append(cell)
-            self.cells.append(row_cells)
-        assert width is not None
-        self.dimensions = Dimensions(width=width, height=len(rows))
+    def __init__(self, cells: np.ndarray, dimensions: Dimensions, symbols: tuple[str, ...]) -> None:
+        self.cells = cells
+        self.dimensions = dimensions
+        self.symbols = symbols
 
     @staticmethod
-    def parse(file: TextIO, floor: str|None=None) -> Grid:
+    def parse(file: TextIO, symbols: tuple[str, ...] | list[str] | str, unique: str | None) -> tuple[Grid, Point | None]:
+        num_symbols: int = len(symbols)
+        assert (num_symbols >= 2), 'too few symbols'
+        assert (num_symbols <= 256), 'too many symbols'
+        assert (len(set(symbols)) == num_symbols), 'duplicate in symbols'
+        assert ('\n' not in symbols), 'symbols contains new line character'
+        if type(symbols) != tuple:
+            symbols = tuple(symbols)
+        assert type(symbols) == tuple
+        for symbol in symbols:
+            assert (type(symbol) == str and len(symbol) == 1), f'invalid symbol: {symbol!r}'
+        if unique is not None:
+            assert (unique in symbols), f'unique symbol {unique!r} no in symbol list'
+        unique_location = None
         rows: list[str] = []
+        num_cols: int | None = None
         for line in file:
             line = line.strip('\n')
             if not line:
                 break
+            for symbol in line:
+                assert (symbol in symbols), f'unexpected symbol: {symbol!r}'
             rows.append(line)
-        return Grid(rows, floor=floor)
+            if num_cols is not None:
+                assert (len(line) == num_cols), ''
+            num_cols = len(line)
+        num_rows: int = len(rows)
+        assert num_rows > 0 and num_cols is not None and num_cols > 0
+        cells = np.zeros((num_rows, num_cols), dtype=np.uint8)
+        assert type(cells) == np.ndarray and len(cells.shape) == 2 and cells.dtype == np.uint8
+        assert cells.shape[0] > 0 and cells.shape[1] > 0
+        for y, row in enumerate(rows):
+            for x, symbol in enumerate(row):
+                try:
+                    if symbol == unique:
+                        if unique_location is not None:
+                            raise AssertionError(f'unique symbol {unique!r} is not unique')
+                        unique_location = Point(x=x, y=y)
+                    cells[y, x] = symbols.index(symbol)
+                except ValueError:
+                    raise AssertionError(f'unexpected symbol populating cells: {symbol!r}')
+        if unique is not None and unique_location is None:
+            raise AssertionError(f'did not find unique symbol {unique!r}')
+        return Grid(cells, Dimensions(width=num_cols, height=num_rows), symbols), unique_location
 
     def __str__(self) -> str:
         rv: str = ''
-        for row in self.cells:
-            for cell in row:
-                rv += cell
+        for y in range(self.cells.shape[0]):
+            for x in range(self.cells.shape[1]):
+                rv += self.symbols[self.cells[y, x]]
             rv += '\n'
         return rv.strip('\n')
 
-    def __setitem__(self, position: Point, symbol: str) -> None:
-        assert type(position) == Point
-        assert position in self.dimensions
-        assert type(symbol) == str and len(symbol) == 1
-        self.cells[position.y][position.x] = symbol
+    def __repr__(self) -> str:
+        return f'Grid({self.cells!r}, {self.dimensions!r}, {self.symbols!r})'
 
-    def __getitem__(self, position: Point) -> str:
-        assert type(position) == Point
-        assert position in self.dimensions
-        return self.cells[position.y][position.x]
+    def __getitem__(self, point: Point) -> str:
+        assert type(point) == Point and point in self.dimensions
+        return self.symbols[self.cells[point.y, point.x]]
 
-    def collision_check(self, position: Point) -> bool:
-        assert self.floor is not None
-        assert type(position) == Point
-        assert position in self.dimensions
-        return self.cells[position.y][position.x] != self.floor
+    def __setitem__(self, point: Point, symbol: str) -> None:
+        assert type(point) == Point and point in self.dimensions
+        assert (symbol in self.symbols), f'cannot set to unexpected new symbol {symbol!r}'
+        try:
+            number: int = self.symbols.index(symbol)
+        except ValueError:
+            raise AssertionError(f'unexpected symbol populating cells: {symbol!r}')
+        self.cells[point.y, point.x] = number
+
+    def copy(self) -> Grid:
+        return Grid(self.cells.copy(), self.dimensions, self.symbols)
+
+    def __eq__(self, other: Grid) -> bool:
+        return (type(other) == Grid
+                and self.symbols == other.symbols
+                and self.dimensions == other.dimensions
+                and self.cells.dtype is self.cells.dtype
+                and np.array_equal(self.cells, other.cells))
+
+    def freeze(self) -> FrozenGrid:
+        return FrozenGrid(self)
+
+
+class FrozenGrid:
+
+    cells: np.ndarray
+    dimensions: Dimensions
+    symbols: tuple[str, ...]
+
+    def __init__(self, grid: Grid) -> None:
+        self.cells = grid.cells.copy()
+        self.dimensions = grid.dimensions
+        self.symbols = grid.symbols
+
+    def __str__(self) -> str:
+        rv: str = ''
+        for y in range(self.cells.shape[0]):
+            for x in range(self.cells.shape[1]):
+                rv += self.symbols[self.cells[y, x]]
+            rv += '\n'
+        return rv.strip('\n')
+
+    def __repr__(self) -> str:
+        return f'FrozenGrid(Grid({self.cells!r}, {self.dimensions!r}, {self.symbols!r}))'
+
+    def __getitem__(self, point: Point) -> str:
+        assert type(point) == Point and point in self.dimensions
+        return self.symbols[self.cells[point.y, point.x]]
+
+    def __hash__(self) -> int:
+        return hash((tuple(self.cells.reshape((-1, ))), self.symbols))
+
+    def __eq__(self, other: Grid) -> bool:
+        return (type(other) == Grid
+                and self.symbols == other.symbols
+                and self.dimensions == other.dimensions
+                and self.cells.dtype is self.cells.dtype
+                and np.array_equal(self.cells, other.cells))
+
+    def unfreeze(self) -> Grid:
+        return Grid(self.cells, self.dimensions, self.symbols).copy()
