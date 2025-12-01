e7f531c377d0a7b3e73e576d696581f9b8ce3784
Dr Lee A. Christie <l.a.christie@rgu.ac.uk>
2025-12-01 08:18:39 +0000

    set day

--- solvers/days.py
+++ solvers/days.py
@@ -1,13 +1,11 @@
 from typing import Any, Callable, Iterator
 
 from .day01 import *
-from .day02 import *
 
 __all__ = ['SOLVER_LIST', 'SolverType']
 
 type SolverType = Callable[[Iterator[str]], Iterator[Any]]
 
 SOLVER_LIST: dict[int, tuple[str, SolverType]] = {
-    1: ('First Placeholder Day', solve01),
-    2: ('Second Placeholder Day', solve02)
+    1: ('Secret Entrance', solve01)
 }
