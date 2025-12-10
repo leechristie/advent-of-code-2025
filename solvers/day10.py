0568a7c405195f8f49706d264134f90b7f86a8ec
Dr Lee A. Christie <l.a.christie@rgu.ac.uk>
2025-12-10 14:03:25 +0000

    some tracking for another attempt

--- solvers/day10.py
+++ solvers/day10.py
@@ -13,7 +13,7 @@ __all__ = ['solve10']
 def parse_machine(line: str) -> tuple[list[bool], list[tuple[int, ...]], list[int]]:
     required_lights: list[bool]
     buttons: list[tuple[int, ...]] = []
-    required_joltage: list[int]
+    required_joltages: list[int]
     tokens: list[str] = line.split(' ')
     light_str, button_tokens, joltage_str = tokens[0], tokens[1:-1], tokens[-1]
     light_str = light_str.removeprefix('[').removesuffix(']')
@@ -22,8 +22,8 @@ def parse_machine(line: str) -> tuple[list[bool], list[tuple[int, ...]], list[in
     for button_token in button_tokens:
         button_token = button_token.removeprefix('(').removesuffix(')')
         buttons.append(tuple([int(e) for e in button_token.split(',')]))
-    required_joltage = [int(e) for e in joltage_str.split(',')]
-    return required_lights, buttons, required_joltage
+    required_joltages = [int(e) for e in joltage_str.split(',')]
+    return required_lights, buttons, required_joltages
 
 
 def parse_machines(lines: Iterator[str]) -> Iterator[tuple[list[bool], list[tuple[int, ...]], list[int]]]:
@@ -79,28 +79,28 @@ def with_button_pressed(goal: list[int] | tuple[int, ...], button: tuple[int, ..
 NO_SOLUTION: int = -1
 
 
-def fewest_presses_for_joltage_memoized(required_joltage: list[int], buttons: list[tuple[int, ...]]) -> int:
+def fewest_presses_for_joltage_memoized(required_joltages: list[int], buttons: list[tuple[int, ...]]) -> int:
     memo: dict[tuple[int, ...], int] = {}
     def __fewest_presses_for_joltage() -> int | None:
-        t: tuple[int, ...] = tuple(required_joltage)
+        t: tuple[int, ...] = tuple(required_joltages)
         if t in memo:
             return memo[t]
-        if sum(required_joltage) == 0:
+        if sum(required_joltages) == 0:
             memo[t] = 0
             return 0
         lowest_remaining: int = NO_SOLUTION
         for button in buttons:
-            if can_press(required_joltage, button):
-                press_button(required_joltage, button)
-                if sum(required_joltage) == 0:
-                    unpress_button(required_joltage, button)
+            if can_press(required_joltages, button):
+                press_button(required_joltages, button)
+                if sum(required_joltages) == 0:
+                    unpress_button(required_joltages, button)
                     memo[t] = 1
                     return 1
                 remaining_presses: int = __fewest_presses_for_joltage()
                 if remaining_presses != NO_SOLUTION:
                     if lowest_remaining == NO_SOLUTION or remaining_presses < lowest_remaining:
                         lowest_remaining = remaining_presses
-                unpress_button(required_joltage, button)
+                unpress_button(required_joltages, button)
         if lowest_remaining != NO_SOLUTION:
             lowest_remaining += 1
         memo[t] = lowest_remaining
@@ -108,9 +108,9 @@ def fewest_presses_for_joltage_memoized(required_joltage: list[int], buttons: li
     return __fewest_presses_for_joltage()
 
 
-def fewest_presses_for_joltage_by_a_star(required_joltage: list[int], buttons: list[tuple[int, ...]]) -> int:
+def fewest_presses_for_joltage_by_a_star(required_joltages: list[int], buttons: list[tuple[int, ...]]) -> int:
 
-    start: tuple[int, ...] = tuple(required_joltage)
+    start: tuple[int, ...] = tuple(required_joltages)
     goal: Callable[[tuple[int, ...]], bool] = lambda x: sum(x) == 0
     heuristic: Callable[[tuple[int, ...]], int] = max
     def neighbours(joltages: tuple[int, ...]) -> list[tuple[tuple[int, ...], int]]:
@@ -123,6 +123,31 @@ def fewest_presses_for_joltage_by_a_star(required_joltage: list[int], buttons: l
     return len(result) - 1
 
 
+def possible_ranges(required_joltages: list[int], buttons: list[tuple[int, ...]]) -> list[tuple[int, int]]:
+    rv: list[list[int]] = [[0, max(required_joltages)] for _ in range(len(buttons))]
+    for index, required_joltage in enumerate(required_joltages):
+        supported_button_ids: list[int] = []
+        for button_id, button in enumerate(buttons):
+            if index in button:
+                supported_button_ids.append(button_id)
+        assert len(supported_button_ids) > 0
+        if len(supported_button_ids) == 1:
+            button_id: int = supported_button_ids[0]
+            press_count: int = required_joltage
+            assert rv[button_id][0] <= press_count
+            assert rv[button_id][1] >= press_count
+            rv[button_id][0] = press_count
+            rv[button_id][1] = press_count
+        else:
+            max_press_count: int = required_joltage
+            for button_id in supported_button_ids:
+                if rv[button_id][1] > max_press_count:
+                    rv[button_id][1] = max_press_count
+                    assert rv[button_id][0] <= rv[button_id][1]
+
+    return [(minimum, maximum) for minimum, maximum in rv]
+
+        
 def solve10(lines: Iterator[str]) -> Iterator[int]:
 
     part1: int = 0
@@ -130,21 +155,23 @@ def solve10(lines: Iterator[str]) -> Iterator[int]:
 
     machines: list[tuple[list[bool], list[tuple[int, ...]], list[int]]] = list(parse_machines(lines))
 
-    for required_lights, buttons, required_joltage in machines:
+    for required_lights, buttons, required_joltages in machines:
         part1 += fewest_presses(required_lights, buttons)
 
     assert (part1 in (7, 494)), f'part1 = {part1}'
     yield part1
     
-    for i, (required_lights, buttons, required_joltage) in enumerate(machines, start=1):
+    for i, (required_lights, buttons, required_joltages) in enumerate(machines, start=1):
         print(f'solving machine {i} of {len(machines)} . . .')
         print('   ', required_lights)
         print('   ', buttons)
-        print('   ', required_joltage)
+        print('   ', required_joltages)
         print('    start :', time.strftime('%X %x %Z'))
         start: float = time.perf_counter()
-        #current: int = fewest_presses_for_joltage_memoized(required_joltage, buttons)
-        current: int = fewest_presses_for_joltage_by_a_star(required_joltage, buttons)
+        #current: int = fewest_presses_for_joltage_memoized(required_joltages, buttons)
+        for button, (minimum, maximum) in zip(buttons, possible_ranges(required_joltages, buttons)):
+            print(f'        button {button} to be pressed {minimum} to {maximum} times')
+        current: int = fewest_presses_for_joltage_by_a_star(required_joltages, buttons)
         taken: float = time.perf_counter() - start
         print(f'    took : {taken / 60:.1f} minutes')
         print(f'    answer: {current}')
