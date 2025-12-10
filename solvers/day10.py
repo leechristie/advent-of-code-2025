798896a0507cc79344a38265fba364d79360fd63
Dr Lee A. Christie <l.a.christie@rgu.ac.uk>
2025-12-10 10:54:14 +0000

    working on part 2 example

--- solvers/day10.py
+++ solvers/day10.py
@@ -1,8 +1,11 @@
 import itertools
+from functools import cache
 from typing import Iterator
 
 
 __all__ = ['solve10']
+
+import numpy as np
 
 
 def parse_machine(line: str) -> tuple[list[bool], list[set[int]], list[int]]:
@@ -47,8 +50,57 @@ def fewest_presses(required_lights: list[bool], buttons: list[set[int]]) -> int:
     raise AssertionError
 
 
+def can_press(goal: list[int], button: set[int]) -> bool:
+    for i in button:
+        if goal[i] == 0:
+            return False
+    return True
+
+
+def press_button(goal: list[int], button: set[int]) -> None:
+    for i in button:
+        goal[i] -= 1
+
+
+def unpress_button(goal: list[int], button: set[int]) -> None:
+    for i in button:
+        goal[i] += 1
+
+
+
+NO_SOLUTION: int = -1
+
+
 def fewest_presses_for_joltage(required_joltage: list[int], buttons: list[set[int]]) -> int:
-    return 0  # TODO
+
+    memo: dict[tuple[int, ...], int] = {}
+
+    def __fewest_presses_for_joltage() -> int | None:
+        t: tuple[int, ...] = tuple(required_joltage)
+        if t in memo:
+            return memo[t]
+        if sum(required_joltage) == 0:
+            memo[t] = 0
+            return 0
+        lowest_remaining: int = NO_SOLUTION
+        for button in buttons:
+            if can_press(required_joltage, button):
+                press_button(required_joltage, button)
+                if sum(required_joltage) == 0:  # opt
+                    unpress_button(required_joltage, button)
+                    memo[t] = 1
+                    return 1            # opt
+                remaining_presses: int = __fewest_presses_for_joltage()
+                if remaining_presses != NO_SOLUTION:
+                    if lowest_remaining == NO_SOLUTION or remaining_presses < lowest_remaining:
+                        lowest_remaining = remaining_presses
+                unpress_button(required_joltage, button)
+        if lowest_remaining != NO_SOLUTION:
+            lowest_remaining += 1
+        memo[t] = lowest_remaining
+        return lowest_remaining
+
+    return __fewest_presses_for_joltage()
 
 
 def solve10(lines: Iterator[str]) -> Iterator[int]:
@@ -60,6 +112,10 @@ def solve10(lines: Iterator[str]) -> Iterator[int]:
     buttons: list[set[int]]
     required_joltage: list[int]
     for required_lights, buttons, required_joltage in parse_machines(lines):
+        print('solving:')
+        print('   ', required_lights)
+        print('   ', buttons)
+        print('   ', required_joltage)
         part1 += fewest_presses(required_lights, buttons)
         part2 += fewest_presses_for_joltage(required_joltage, buttons)
 
