f54ee8f1e715cd8aa34614103c4ad9f51beee639
Dr Lee A. Christie <l.a.christie@rgu.ac.uk>
2025-12-11 08:43:20 +0000

    optimising

--- solvers/day11.py
+++ solvers/day11.py
@@ -10,45 +10,59 @@ def parse_device(line: str, devices: dict[str, set[str]]) -> None:
     devices[device_id] = set(outputs.split(' '))
 
 
-def parse_devices(lines: Iterator[str]) -> dict[str, set[str]]:
+def parse_devices_str(lines: Iterator[str]) -> dict[str, set[str]]:
     rv: dict[str, set[str]] = {}
     for line in lines:
         parse_device(line, rv)
     return rv
 
 
-def solve_part1(devices: dict[str, set[str]]) -> int:
+def parse_devices(lines: Iterator[str], poi: tuple[str, ...]) -> tuple[dict[int, tuple[int, ...]], tuple[int, ...]]:
+    devices_str: dict[str, set[str]] = parse_devices_str(lines)
+    devices: dict[int, tuple[int, ...]] = {}
+    strings_to_ints: dict[str, int] = {}
+    ints_to_strings: list[str] = []
+    for i, s in enumerate(set(devices_str.keys()) | set(poi)):
+        ints_to_strings.append(s)
+        strings_to_ints[s] = i
+    poi_ints: tuple[int, ...] = tuple([strings_to_ints[p] for p in poi])
+    for device, outputs in devices_str.items():
+        devices[strings_to_ints[device]] = tuple([strings_to_ints[d] for d in outputs])
+    return devices, poi_ints
+
+
+def solve_part1(DEVICE: dict[int, tuple[int, ...]], YOU: int, OUT: int) -> int:
     @cache
-    def num_paths(start: str) -> int:
+    def num_paths(start: int) -> int:
         rv: int = 0
-        if start == 'out':
+        if start == OUT:
             rv = 1
         else:
-            for output in devices[start]:
+            for output in DEVICE[start]:
                 rv += num_paths(output)
         return rv
-    return num_paths('you')
+    return num_paths(YOU)
 
 
-def solve_part2(devices: dict[str, set[str]]) -> int:
+def solve_part2(DEVICE: dict[int, tuple[int, ...]], SVR: int, DAC: int, FFT: int, OUT: int) -> int:
 
     @cache
-    def num_paths(start: str, indent=0) -> tuple[int, int, int, int]:
+    def num_paths(start: int) -> tuple[int, int, int, int]:
         empty: int = 0
         dac: int = 0
         fft: int = 0
         dac_fft: int = 0
-        if start == 'out':
+        if start == OUT:
             empty += 1
         else:
-            for output in devices[start]:
-                empty_paths, dac_paths, fft_paths, dac_fft_paths = num_paths(output, indent=indent+1)
-                if start == 'dac':
+            for output in DEVICE[start]:
+                empty_paths, dac_paths, fft_paths, dac_fft_paths = num_paths(output)
+                if start == DAC:
                     dac += empty_paths
                     dac += dac_paths
                     dac_fft += fft_paths
                     dac_fft += dac_fft_paths
-                elif start == 'fft':
+                elif start == FFT:
                     fft += empty_paths
                     dac_fft += dac_paths
                     fft += fft_paths
@@ -60,24 +74,22 @@ def solve_part2(devices: dict[str, set[str]]) -> int:
                     dac_fft += dac_fft_paths
         return empty, dac, fft, dac_fft
 
-    _, _, _, rv = num_paths('svr')
+    _, _, _, rv = num_paths(SVR)
     return rv
 
 
 def solve11(lines: Iterator[str],
             example_b_lines: Iterator[str] | None = None) -> Iterator[int]:
 
-    devices: dict[str, set[str]]
-
-    devices = parse_devices(lines)
-    part1: int = solve_part1(devices)
+    devices, (you, svr, dac, fft, out) = parse_devices(lines, ('you', 'svr', 'dac', 'fft', 'out'))
+    part1: int = solve_part1(devices, you, out)
     assert (part1 in (5, 788)), f'{part1 = }, expected 5 (example) or 788 (input)'
-    yield solve_part1(devices)
+    yield part1
 
     # puzzle has different Part 2 input for the example :/
     if example_b_lines is not None:
-        devices = parse_devices(example_b_lines)
+        devices, (you, svr, dac, fft, out) = parse_devices(example_b_lines, ('you', 'svr', 'dac', 'fft', 'out'))
 
-    part2: int = solve_part2(devices)
+    part2: int = solve_part2(devices, svr, dac, fft, out)
     assert ((part1, part2) in ((5, 2), (788, 316291887968000))), f'{part2 = }'
     yield part2
