d3f1211d7ca17c4966ef218486af2b2656fe0453
Dr Lee A. Christie <l.a.christie@rgu.ac.uk>
2025-12-11 08:47:57 +0000

    optimised day 11

--- solvers/day11.py
+++ solvers/day11.py
@@ -1,4 +1,3 @@
-from functools import cache
 from typing import Iterator
 
 
@@ -17,7 +16,7 @@ def parse_devices_str(lines: Iterator[str]) -> dict[str, set[str]]:
     return rv
 
 
-def parse_devices(lines: Iterator[str], poi: tuple[str, ...]) -> tuple[dict[int, tuple[int, ...]], tuple[int, ...]]:
+def parse_devices(lines: Iterator[str], poi: tuple[str, ...]) -> tuple[dict[int, tuple[int, ...]], int, tuple[int, ...]]:
     devices_str: dict[str, set[str]] = parse_devices_str(lines)
     devices: dict[int, tuple[int, ...]] = {}
     strings_to_ints: dict[str, int] = {}
@@ -28,26 +27,32 @@ def parse_devices(lines: Iterator[str], poi: tuple[str, ...]) -> tuple[dict[int,
     poi_ints: tuple[int, ...] = tuple([strings_to_ints[p] for p in poi])
     for device, outputs in devices_str.items():
         devices[strings_to_ints[device]] = tuple([strings_to_ints[d] for d in outputs])
-    return devices, poi_ints
+    return devices, len(strings_to_ints), poi_ints
 
 
-def solve_part1(DEVICE: dict[int, tuple[int, ...]], YOU: int, OUT: int) -> int:
-    @cache
+def solve_part1(DEVICE: dict[int, tuple[int, ...]], NUM_DEVICES: int, YOU: int, OUT: int) -> int:
+    memo: list[int] = [-1] * NUM_DEVICES
     def num_paths(start: int) -> int:
+        m = memo[start]
+        if memo[start] != -1:
+            return m
         rv: int = 0
         if start == OUT:
             rv = 1
         else:
             for output in DEVICE[start]:
                 rv += num_paths(output)
+        memo[start] = rv
         return rv
     return num_paths(YOU)
 
 
-def solve_part2(DEVICE: dict[int, tuple[int, ...]], SVR: int, DAC: int, FFT: int, OUT: int) -> int:
-
-    @cache
+def solve_part2(DEVICE: dict[int, tuple[int, ...]], NUM_DEVICES: int, SVR: int, DAC: int, FFT: int, OUT: int) -> int:
+    memo: list[tuple[int, int, int, int] | None] = [None] * NUM_DEVICES
     def num_paths(start: int) -> tuple[int, int, int, int]:
+        m = memo[start]
+        if memo[start] is not None:
+            return m
         empty: int = 0
         dac: int = 0
         fft: int = 0
@@ -72,7 +77,9 @@ def solve_part2(DEVICE: dict[int, tuple[int, ...]], SVR: int, DAC: int, FFT: int
                     dac += dac_paths
                     fft += fft_paths
                     dac_fft += dac_fft_paths
-        return empty, dac, fft, dac_fft
+        rv: tuple[int, int, int, int] = empty, dac, fft, dac_fft
+        memo[start] = rv
+        return rv
 
     _, _, _, rv = num_paths(SVR)
     return rv
@@ -81,15 +88,12 @@ def solve_part2(DEVICE: dict[int, tuple[int, ...]], SVR: int, DAC: int, FFT: int
 def solve11(lines: Iterator[str],
             example_b_lines: Iterator[str] | None = None) -> Iterator[int]:
 
-    devices, (you, svr, dac, fft, out) = parse_devices(lines, ('you', 'svr', 'dac', 'fft', 'out'))
-    part1: int = solve_part1(devices, you, out)
-    assert (part1 in (5, 788)), f'{part1 = }, expected 5 (example) or 788 (input)'
-    yield part1
+    devices, num_devices, (you, svr, dac, fft, out) = parse_devices(lines, ('you', 'svr', 'dac', 'fft', 'out'))
+
+    yield solve_part1(devices, num_devices, you, out)
 
     # puzzle has different Part 2 input for the example :/
     if example_b_lines is not None:
-        devices, (you, svr, dac, fft, out) = parse_devices(example_b_lines, ('you', 'svr', 'dac', 'fft', 'out'))
+        devices, num_devices, (you, svr, dac, fft, out) = parse_devices(example_b_lines, ('you', 'svr', 'dac', 'fft', 'out'))
 
-    part2: int = solve_part2(devices, svr, dac, fft, out)
-    assert ((part1, part2) in ((5, 2), (788, 316291887968000))), f'{part2 = }'
-    yield part2
+    yield solve_part2(devices, num_devices, svr, dac, fft, out)
